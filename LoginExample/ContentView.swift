4694969b57bbef50a4648a3c6c9bf159220ed8e0
Eduardo Finotti <eduardo.finotti@jamesdelivery.com.br>
2022-01-12 13:26:34 -0300

    Initial Commit

---
+++ LoginExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginExample
+//
+//  Created by Eduardo Finotti on 12/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginExample/LoginExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginExampleApp.swift
+//  LoginExample
+//
+//  Created by Eduardo Finotti on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
